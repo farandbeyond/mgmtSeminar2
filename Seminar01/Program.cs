794fdbad484b9904e55eb02e86e8f3931ba6182b
Connor <connorjh4@gmail.com>
2025-09-25 14:33:13 -0400

    moved over to array fully, now functions once more

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -69,7 +69,7 @@
 
             for (int i = 0; i < 6; i++)
             {
-                Console.WriteLine("Enter Character Agility: ");
+                Console.WriteLine($"Enter Character Stat: ");
                 stats[i] = Convert.ToInt32(Console.ReadLine());
                 stats[i] = Math.Clamp(stats[i], 15, 50);
             }
@@ -123,33 +123,36 @@
             int Resolve;
 
             //calculate them, using the primary attributes collected above
-            //Awareness = stats[0] + stats[3];
+            //Awareness = Agility + Perception
+            Awareness = stats[(int)STATS.Agility] + stats[(int)STATS.Perception];
             //Toughness = Strength + Vigour;
+            Toughness = stats[(int)STATS.Strength] + stats[(int)STATS.Vigour];
             //Resolve = Intellect + Will;
+            Resolve = stats[(int)STATS.Intellect] + stats[(int)STATS.Will];
 
 
-            ////    Use Console.Clear() before displaying.
-            ////Change Console.ForegroundColor and/ or Console.BackgroundColor.
-            //Console.BackgroundColor = ConsoleColor.Gray;
-            //Console.ForegroundColor = ConsoleColor.DarkBlue;
-            //Console.Clear();
-            ////Add a custom Console.Title.
-            //Console.Title = "Character Sheet: " + characterName;
+            //    Use Console.Clear() before displaying.
+            //Change Console.ForegroundColor and/ or Console.BackgroundColor.
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            //Add a custom Console.Title.
+            Console.Title = "Character Sheet: " + characterName;
 
-            ////Display nicely formatted output(extra \n or multiple Console.WriteLine).
-            ////Create ASCII dividers(dashed lines) to separate the CharacterName, Primary Attributes and Secondary Attributes.
-            ////formatted strings because im fancy like that
-            //Console.WriteLine("\n{0}", characterName);
-            //Console.WriteLine("---------------------------");
-            //Console.WriteLine("AGI: {0,2:D2} | STR: {1,2:D2}", Agility, Strength);
-            //Console.WriteLine("VIG: {0,2:D2} | PER: {1,2:D2}", Vigour, Perception);
-            //Console.WriteLine("INT: {0,2:D2} | WIL: {1,2:D2}", Intellect, Will);
-            //Console.WriteLine("---------------------------");
-            ////these can hit 100, but since theres no second column to worry about, no min length required
-            //Console.WriteLine("AWARENESS: {0,3}", Awareness);
-            //Console.WriteLine("TOUGHNESS: {0,3}", Toughness);
-            //Console.WriteLine("  RESOLVE: {0,3}", Resolve);
-            //Console.WriteLine("---------------------------");
+            //Display nicely formatted output(extra \n or multiple Console.WriteLine).
+            //Create ASCII dividers(dashed lines) to separate the CharacterName, Primary Attributes and Secondary Attributes.
+            //formatted strings because im fancy like that
+            Console.WriteLine("\n{0}", characterName);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("AGI: {0,2:D2} | STR: {1,2:D2}", stats[(int)STATS.Agility], stats[(int)STATS.Strength]);
+            Console.WriteLine("VIG: {0,2:D2} | PER: {1,2:D2}", stats[(int)STATS.Vigour], stats[(int)STATS.Perception]);
+            Console.WriteLine("INT: {0,2:D2} | WIL: {1,2:D2}", stats[(int)STATS.Intellect], stats[(int)STATS.Will]);
+            Console.WriteLine("---------------------------");
+            //these can hit 100, but since theres no second column to worry about, no min length required
+            Console.WriteLine("AWARENESS: {0,3}", Awareness);
+            Console.WriteLine("TOUGHNESS: {0,3}", Toughness);
+            Console.WriteLine("  RESOLVE: {0,3}", Resolve);
+            Console.WriteLine("---------------------------");
 
 
         }
